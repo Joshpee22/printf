54ac1f5e3f10e3021eb62fe205d0661e54fd399f
joshpee22 <owobujoshua2017@gmail.com>
2023-04-24 15:03:18 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,90 +1,49 @@
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
 #include "main.h"
 
-int identify(char, va_list);
-int _printf(const char *format, ...);
-
 /**
- * _printf - recreating printf
- *
- * @format: formattting of output
- *
- * Return: lint
+ *  _printf - prints %c and %s format specifier
+ *  @format: format string
+ *  Return: number of printed charaters
  */
 
 int _printf(const char *format, ...)
 {
-	int i = 0, len = 0, print_flag = 0, flag = 0;
-	va_list args;
+	unsigned int i = 0, count = 0;
+	va_list valist;
+	int (*f)(va_list);
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(args, format);
-	for (i = 0; format[i] != '\0'; i++)
+	va_start(valist, format);
+
+	while (format[i])
 	{
-		if (format[i] != '%')
+		for (; format[i] != '%' && format[i]; i++)
 		{
-			len += _putchar(format[i]);
-			continue;
-		}
-		if (format[i + 1] == '%')
-		{
-			len += _putchar('%');
-			i++;
-			flag = 1;
-			continue;
+			_putchar(format[i]);
+			count++;
 		}
 
-		if (format[i + 1] == '\0' && format[i] == '%')
+		if (!format[i])
+			return (count);
+
+		f = check_specifier(&format[i + 1]);
+		if (f != NULL)
+		{
+			count += f(valist);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
 			return (-1);
-		if (format[i + 1] == '\0')
-			return (len);
-
-		print_flag = identify(format[i + 1], args);
-
-		if (print_flag == -1 || print_flag != 0)
+		_putchar(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+			i += 2;
+		else
 			i++;
-		if (print_flag > 0)
-			len += print_flag;
-		if (print_flag == 0 && flag == 0)
-		{
-			_putchar('%');
-			len++;
-		}
 	}
-	va_end(args);
-	return (len);
-}
-
-/**
- * identify - identify the specifier used after %
- * @ch: char of specifier
- * @args: va_list of variables
- * Return: int / len
- */
-int identify(char ch, va_list args)
-{
-	int i;
-	d_type data_type[] = {
-		{"c", print_char},
-		{"s", print_str},
-		{"d", print_int},
-		{"i", print_int},
-		{"b", print_ui_b},
-		{"u", print_unsigned},
-		{"x", print_x},
-		{"X", print_X},
-		{"R", print_rot},
-		{NULL, NULL}
-		};
-
-	for (i = 0; data_type[i].sp != NULL; i++)
-	{
-		if (data_type[i].sp[0] == ch)
-			return (data_type[i].f(args));
-	}
-	return (0);
+	va_end(valist);
+	return (count);
 }
--- main.h
+++ main.h
@@ -1,29 +1,41 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 /**
- * struct d_type - structure of function pointers
- * @sp: specifier char to match function with
- * @f: function to invoke
+ * struct print_struct - structure for printing various types
+ * @t: type to print
+ * @f: function to print
  */
-typedef struct d_type
+
+typedef struct print_struct
 {
-	char *sp;
+	char *t;
 	int (*f)(va_list);
-} d_type;
+} print_type;
+
 
 int _printf(const char *format, ...);
-int print_char(va_list);
-int print_str(va_list);
-int _strlen(char *);
-int _putchar(char);
-int print_int(va_list);
-void to_binary(unsigned int, int*);
-int print_ui_b(va_list);
-int print_unsigned(va_list);
-int print_X(va_list);
-int print_x(va_list);
-int print_rot(va_list);
+int _putchar(char c);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_b(va_list b);
+int print_u(va_list u);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_S(va_list S);
+int print_p(va_list p);
+int (*check_specifier(const char *format))(va_list);
+int print_hex(unsigned int n, unsigned int c);
+int hex_print(char c);
+int print_r(va_list r);
+int print_R(va_list R);
+
 #endif
