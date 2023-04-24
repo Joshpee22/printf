464eb4cba690c4a9b27a882b2b85146db42cfa95
joshpee22 <owobujoshua2017@gmail.com>
2023-04-24 12:37:28 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,51 +1,90 @@
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
 #include "main.h"
 
+int identify(char, va_list);
+int _printf(const char *format, ...);
+
 /**
- * _printf - formatted output conversion and print data.
- * @format: input string.
+ * _printf - recreating printf
  *
- * Return: number of chars printed.
+ * @format: formattting of output
+ *
+ * Return: lint
  */
+
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0, ibuf = 0;
-	va_list arguments;
-	int (*function)(va_list, char *, unsigned int);
-	char *buffer;
+	int i = 0, len = 0, print_flag = 0, flag = 0;
+	va_list args;
 
-	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
-	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+	if (format == NULL)
 		return (-1);
-	if (!format[i])
-		return (0);
-	for (i = 0; format && format[i]; i++)
+
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] != '%')
 		{
-			if (format[i + 1] == '\0')
-			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
-				return (-1);
-			}
-			else
-			{	function = get_print_func(format, i + 1);
-				if (function == NULL)
-				{
-					if (format[i + 1] == ' ' && !format[i + 2])
-						return (-1);
-					handl_buf(buffer, format[i], ibuf), len++, i--;
-				}
-				else
-				{
-					len += function(arguments, buffer, ibuf);
-					i += ev_print_func(format, i + 1);
-				}
-			} i++;
+			len += _putchar(format[i]);
+			continue;
 		}
-		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
-			;
+		if (format[i + 1] == '%')
+		{
+			len += _putchar('%');
+			i++;
+			flag = 1;
+			continue;
+		}
+
+		if (format[i + 1] == '\0' && format[i] == '%')
+			return (-1);
+		if (format[i + 1] == '\0')
+			return (len);
+
+		print_flag = identify(format[i + 1], args);
+
+		if (print_flag == -1 || print_flag != 0)
+			i++;
+		if (print_flag > 0)
+			len += print_flag;
+		if (print_flag == 0 && flag == 0)
+		{
+			_putchar('%');
+			len++;
+		}
 	}
-	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	va_end(args);
 	return (len);
+}
+
+/**
+ * identify - identify the specifier used after %
+ * @ch: char of specifier
+ * @args: va_list of variables
+ * Return: int / len
+ */
+int identify(char ch, va_list args)
+{
+	int i;
+	d_type data_type[] = {
+		{"c", print_char},
+		{"s", print_str},
+		{"d", print_int},
+		{"i", print_int},
+		{"b", print_ui_b},
+		{"u", print_unsigned},
+		{"x", print_x},
+		{"X", print_X},
+		{"R", print_rot},
+		{NULL, NULL}
+		};
+
+	for (i = 0; data_type[i].sp != NULL; i++)
+	{
+		if (data_type[i].sp[0] == ch)
+			return (data_type[i].f(args));
+	}
+	return (0);
 }
