d3d846e4e6fb6b5b574ab4ea8801de6216300c0e
Muliro1 <mulirokhaemba@gmail.com>
2023-04-27 01:16:45 +0300

    debug

--- print-functions.c
+++ print-functions.c
@@ -9,21 +9,21 @@
  */
 int print_char(va_list ap, params_t *params)
 {
-	char pad_char = ' ';
-	unsigned int pad = 1, sum = 0, ch = va_arg(ap, int);
+	char padd_char = ' ';
+	unsigned int padd = 1, sum = 0, ch = va_arg(ap, int);
 
 	if (params->minus_flag)
 		sum += _putchar(ch);
-	while (pad++ < params->width)
-		sum += _putchar(pad_char);
+	while (padd++ < params->width)
+		sum += _putchar(padd_char);
 	if (!params->minus_flag)
 		sum += _putchar(ch);
 	return (sum);
 }
 
 /**
- * print_int - prints integer
- * @ap: argument pointer
+ * print_int - prints int
+ * @ap: arg pointer
  * @params: the parameters struct
  *
  * Return: number chars printed
@@ -42,7 +42,7 @@ int print_int(va_list ap, params_t *params)
 }
 
 /**
- * print_string - prints string
+ * print_string - prints str
  * @ap: argument pointer
  * @params: the parameters struct
  *
@@ -50,32 +50,32 @@ int print_int(va_list ap, params_t *params)
  */
 int print_string(va_list ap, params_t *params)
 {
-	char *str = va_arg(ap, char *), pad_char = ' ';
-	unsigned int pad = 0, sum = 0, i = 0, j;
+	char *str = va_arg(ap, char *), padd_char = ' ';
+	unsigned int padd = 0, sum = 0, i = 0, j;
 
 	(void)params;
 	switch ((int)(!str))
 		case 1:
 			str = NULL_STRING;
 
-	j = pad = _strlen(str);
-	if (params->precision < pad)
-		j = pad = params->precision;
+	j = padd = _strlen(str);
+	if (params->precision < padd)
+		j = padd = params->precision;
 
 	if (params->minus_flag)
 	{
 		if (params->precision != UINT_MAX)
-			for (i = 0; i < pad; i++)
+			for (i = 0; i < padd; i++)
 				sum += _putchar(*str++);
 		else
 			sum += _puts(str);
 	}
 	while (j++ < params->width)
-		sum += _putchar(pad_char);
+		sum += _putchar(padd_char);
 	if (!params->minus_flag)
 	{
 		if (params->precision != UINT_MAX)
-			for (i = 0; i < pad; i++)
+			for (i = 0; i < padd; i++)
 				sum += _putchar(*str++);
 		else
 			sum += _puts(str);
@@ -84,9 +84,9 @@ int print_string(va_list ap, params_t *params)
 }
 
 /**
- * print_percent - prints string
- * @ap: argument pointer
- * @params: the parameters struct
+ * print_percent - prints str
+ * @ap: argument point
+ * @params: the parameters stru
  *
  * Return: number chars printed
  */
